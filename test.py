c3114eef08fec71c38ca8abe049579413f03f5d9
lovetonight <89431241+lovetonight@users.noreply.github.com>
2021-12-25 16:37:09 +0700

    upload

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-p=0
-print(p)
+import re
+regex1=re.compile(r'anh (.*) dep')
+demo='''anh Luan dep trai
+anh Linh dep trai nua
+anh Hoan dep trai nua'''
+kq=regex1.findall(demo)
+#name=kq.group(0)
+print(kq[1:3])
